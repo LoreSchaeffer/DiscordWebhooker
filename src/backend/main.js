1751e01c9d2efece97250b6287cd1fec3239d8ae
LoreSchaeffer <lm.lorenzo@outlook.it>
2022-10-19 10:51:19 +0200

    Initial commit

---
+++ src/backend/main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const DEV_TOOLS = false;
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    icon: 'icon.png',
+    width: 1280,
+    height: 720,
+    minWidth: 1280,
+    minHeight: 720,
+    frame: true,
+    autoHideMenuBar: true,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('src/index.html');
+
+  win.once('ready-to-show', () => {
+    win.show();
+    if (DEV_TOOLS) win.webContents.openDevTools();
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
